d8a8f42afbf0fc05bde2144cd48e37d6c8e00778
Rustam Karimov <kerimov.rustam@live.ru>
2023-12-01 16:09:11 +0100

    Update 2018 day 1 task

--- tasks/2018/day_1_solutions.py
+++ tasks/2018/day_1_solutions.py
@@ -1,8 +1,5 @@
 def part_1(_input: str):
     res = 0
     for line in _input.splitlines():
-        if line.startswith('+'):
-            res += int(line[1:])
-        else:
-            res -= int(line[1:])
+        res = res + int(line[1:]) if line.startswith('+') else res - int(line[1:])
     return res
